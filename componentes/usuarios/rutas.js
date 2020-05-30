59b7788f44ae055e7c250388c0f2c869a5f465ed
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-05-29 21:21:38 -0500

    Agregado modulo de autentición y autorización.

--- componentes/usuarios/rutas.js
+++ componentes/usuarios/rutas.js
@@ -3,11 +3,12 @@ const router = express.Router()
 const Usuario = require('./modelo')
 const { responder } = require('../../utilidades/funciones')
 const bcrypt = require('bcryptjs')
+const { crearToken, middlewareDeAutorizacion } = require('../../utilidades/autenticacion')
 
 const multer = require('multer')
 const subidaDeArchivos = multer({ dest: 'avatares/' })
 
-router.get('/', function(solicitud, respuesta) {
+router.get('/', middlewareDeAutorizacion, function(solicitud, respuesta) {
   // El segundo parametro solo es necesario cuando yo necesito traer unos campos especificos, es decir, no todos
   // Para traer Todos los campos Usuario.find(solicitud.query, function(error, usuarios) {
   // Para traer solo el campo "nombre" Usuario.find(solicitud.query, ['nombre'], function(error, usuarios) {
@@ -17,8 +18,6 @@ router.get('/', function(solicitud, respuesta) {
 })
 
 router.post('/', subidaDeArchivos.single('avatar'), function (solicitud, respuesta) {
-  console.log('solicitud.body ', solicitud.body)
-
   const datosUsuario = {
     avatar: `${solicitud.protocol}://${solicitud.get('host')}/${solicitud.file.destination}${solicitud.file.filename}`,
     nombre: solicitud.body.nombre,
@@ -30,6 +29,19 @@ router.post('/', subidaDeArchivos.single('avatar'), function (solicitud, respues
   nuevoUsuario.save(function(error, usuarioCreado) {
     responder(error, respuesta, usuarioCreado)
   })
+})
+
+router.post('/inicio_de_sesion', function (solicitud, respuesta) {
+  Usuario.findOne({ correoElectronico: solicitud.body.correoElectronico }, function (error, usuario) {
+    const mensajeEnCasoDeError = 'Ni el correo, ni la contraseña coinciden'
+
+    if (error || !bcrypt.compareSync(solicitud.body.contrasena, usuario.contrasenaEncriptada)) {
+      responder(error, respuesta, null, mensajeEnCasoDeError, 401)
+    } else {
+      responder(error, respuesta, { jwt: crearToken(usuario) })
+    }
+  })
+
 })
 
 module.exports = router
---
+++ utilidades/autenticacion.js
@@ -0,0 +1,41 @@
+const jwt = require('jwt-simple')
+const moment = require('moment')
+const SECRET = 'misecreto'
+
+const crearToken = function (usuario) {
+  const payload = {
+    id: usuario._id,
+    nombre: usuario.nombre,
+    fechaDeExpiracion: moment().add(1, 'day').unix()
+  }
+
+  return jwt.encode(payload, SECRET)
+}
+
+const validarToken = function (token) {
+  try {
+    const payload = jwt.decode(token, SECRET)
+
+    if (payload.fechaDeExpiracion < moment().unix()) {
+      console.error('Este token ya ha expirado')
+      return false
+    }
+  } catch(error) {
+    console.error('Error decodificando token: ', error)
+    return false
+  }
+
+  return true
+}
+
+const middlewareDeAutorizacion = function (solicitud, respuesta, next) {
+  authorization = solicitud.headers.authorization
+
+  if (authorization && validarToken(authorization.split(' ')[1])) {
+    next()
+  } else {
+    responder({}, respuesta, null, 'Usted NO está autorizado para realizar esta acción.', 401)
+  }
+}
+
+module.exports = { crearToken, middlewareDeAutorizacion, validarToken }
