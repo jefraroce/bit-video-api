a23698faee5a04c97f3d324add218da06315c149
yeison Herrera <yeison.herrera@gmail.com>
2020-06-03 20:41:12 -0500

    API con registro y logueo

--- componentes/usuarios/usuario.controlador.js
+++ componentes/usuarios/usuario.controlador.js
@@ -1,42 +1,66 @@
-const Usuario = require('./usuario.modelo');
-const { responder } = require('../../utilidades/funciones')
+const Usuario = require("./usuario.modelo");
+const { responder } = require("../../utilidades/funciones");
+const bcrypt = require("bcryptjs");
+const {
+    crearToken,
+    middlewareDeAutorizacion,
+    validarToken,
+} = require("../../utilidades/autenticacion");
 
 exports.login = (req, res) => {
-    Usuario.findOne({ correoElectronico: req.body.correoElectronico }, function (error, usuario) {
-        const mensajeEnCasoDeError = 'Ni el correo, ni la contraseña coinciden'
-
-        if (error || !bcrypt.compareSync(req.body.contrasena, usuario.contrasenaEncriptada)) {
-            responder(error, res, null, mensajeEnCasoDeError, 401)
+    Usuario.findOne({ correoElectronico: req.body.correoElectronico }, function(
+        error,
+        usuario
+    ) {
+        const mensajeEnCasoDeError = "Ni el correo, ni la contraseña coinciden";
+        console.log(usuario);
+        if (usuario !== null) {
+            if (
+                error ||
+                !bcrypt.compareSync(req.body.contrasena, usuario.contrasenaEncriptada)
+            ) {
+                responder(error, res, null, mensajeEnCasoDeError, 401);
+            } else {
+                responder(error, res, { jwt: crearToken(usuario) });
+            }
         } else {
-            responder(error, res, { jwt: crearToken(usuario) })
+            responder(error, res, null, mensajeEnCasoDeError, 401);
         }
-    })
-}
+    });
+};
 
 // Create and Save a new Usuario
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.content) {
-        return res.status(400).send({
-            message: "El contenido del Usuario no puede estar vacio"
-        });
-    }
+    //Validate request
+    console.log(req.body.content);
+    console.log(req.body);
+    // if (!req.body.content) {
+    //     return res.status(400).send({
+    //         message: "El contenido del Usuario no puede estar vacio",
+    //     });
+    // }
 
     // Create a Usuario
     const usuario = new Usuario({
-        avatar: `${req.protocol}://${req.get('host')}/${req.file.destination}${req.file.filename}`,
+        avatar: `${req.protocol}://${req.get("host")}/${req.file.destination}${
+      req.file.filename
+    }`,
         nombre: req.body.nombre,
         correoElectronico: req.body.correoElectronico,
-        contrasenaEncriptada: req.body.contrasena === undefined ? null : bcrypt.hashSync(req.body.contrasena)
+        contrasenaEncriptada: req.body.contrasena === undefined ?
+            null :
+            bcrypt.hashSync(req.body.contrasena),
     });
 
     // Save Usuario in the database
-    usuario.save()
-        .then(data => {
+    usuario
+        .save()
+        .then((data) => {
             res.send(data);
-        }).catch(err => {
+        })
+        .catch((err) => {
             res.status(500).send({
-                message: err.message || "Ha ocurrido algun error creando el Usuario."
+                message: "Ha ocurrido algun error creando el Usuario.",
             });
         });
 };
@@ -44,11 +68,12 @@ exports.create = (req, res) => {
 // Retrieve and return all usuarios from the database.
 exports.findAll = (req, res) => {
     Usuario.find()
-        .then(usuarios => {
+        .then((usuarios) => {
             res.send(usuarios);
-        }).catch(err => {
+        })
+        .catch((err) => {
             res.status(500).send({
-                message: err.message || "Ha ocurrido algun error obteniendo los usuarios."
+                message: err.message || "Ha ocurrido algun error obteniendo los usuarios.",
             });
         });
 };
@@ -56,21 +81,22 @@ exports.findAll = (req, res) => {
 // Encontrar un usuario con el usuarioId
 exports.findOne = (req, res) => {
     Usuario.findById(req.params.usuarioId)
-        .then(usuario => {
+        .then((usuario) => {
             if (!usuario) {
                 return res.status(404).send({
-                    message: "Usuario no encontrado con id " + req.params.usuarioId
+                    message: "Usuario no encontrado con id " + req.params.usuarioId,
                 });
             }
             res.send(usuario);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
+        })
+        .catch((err) => {
+            if (err.kind === "ObjectId") {
                 return res.status(404).send({
-                    message: "Usuario no encontrado con id " + req.params.usuarioId
+                    message: "Usuario no encontrado con id " + req.params.usuarioId,
                 });
             }
             return res.status(500).send({
-                message: "Error obteniendo el usuario con id " + req.params.usuarioId
+                message: "Error obteniendo el usuario con id " + req.params.usuarioId,
             });
         });
 };
@@ -80,32 +106,39 @@ exports.update = (req, res) => {
     // Validate Request
     if (!req.body.content) {
         return res.status(400).send({
-            message: "El usuario no puede estar vacio"
+            message: "El usuario no puede estar vacio",
         });
     }
 
     // Encontrar el usuario y actualizarlo con el body del request
-    Usuario.findByIdAndUpdate(req.params.usuarioId, {
-        avatar: `${req.protocol}://${req.get('host')}/${req.file.destination}${req.file.filename}`,
-        nombre: req.body.nombre,
-        correoElectronico: req.body.correoElectronico,
-        contrasenaEncriptada: req.body.contrasena === undefined ? null : bcrypt.hashSync(req.body.contrasena)
-    }, { new: true })
-        .then(usuario => {
+    Usuario.findByIdAndUpdate(
+            req.params.usuarioId, {
+                avatar: `${req.protocol}://${req.get("host")}/${req.file.destination}${
+        req.file.filename
+      }`,
+                nombre: req.body.nombre,
+                correoElectronico: req.body.correoElectronico,
+                contrasenaEncriptada: req.body.contrasena === undefined ?
+                    null :
+                    bcrypt.hashSync(req.body.contrasena),
+            }, { new: true }
+        )
+        .then((usuario) => {
             if (!usuario) {
                 return res.status(404).send({
-                    message: "No se ha encontrado un usuario con el id " + req.params.usuarioId
+                    message: "No se ha encontrado un usuario con el id " + req.params.usuarioId,
                 });
             }
             res.send(usuario);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
+        })
+        .catch((err) => {
+            if (err.kind === "ObjectId") {
                 return res.status(404).send({
-                    message: "No se ha encontrado un usuario con el id " + req.params.usuarioId
+                    message: "No se ha encontrado un usuario con el id " + req.params.usuarioId,
                 });
             }
             return res.status(500).send({
-                message: "Error actualizando el usuario con id " + req.params.usuarioId
+                message: "Error actualizando el usuario con id " + req.params.usuarioId,
             });
         });
 };
@@ -113,21 +146,22 @@ exports.update = (req, res) => {
 // Eliminar un usuario con el id especificado en el request
 exports.delete = (req, res) => {
     Usuario.findByIdAndRemove(req.params.usuarioId)
-        .then(usuario => {
+        .then((usuario) => {
             if (!usuario) {
                 return res.status(404).send({
-                    message: "Usuario no encontrado con id " + req.params.usuarioId
+                    message: "Usuario no encontrado con id " + req.params.usuarioId,
                 });
             }
             res.send({ message: "Usuario eliminado successfully!" });
-        }).catch(err => {
-            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+        })
+        .catch((err) => {
+            if (err.kind === "ObjectId" || err.name === "NotFound") {
                 return res.status(404).send({
-                    message: "Usuario no encontrado con id " + req.params.usuarioId
+                    message: "Usuario no encontrado con id " + req.params.usuarioId,
                 });
             }
             return res.status(500).send({
-                message: "No se puede eliminar el usuario con id " + req.params.usuarioId
+                message: "No se puede eliminar el usuario con id " + req.params.usuarioId,
             });
         });
 };
